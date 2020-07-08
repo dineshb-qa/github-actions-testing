733d3d9d867fde4e359a25bd1c04a50bf3708abf
Dinesh Biradar <dinesh.biradar@forgeahead.io>
2020-07-08 17:36:13 +0530

    updated pom and base class to use WebDriver Manager

--- src/test/java/com/tau/base/TestBase.java
+++ src/test/java/com/tau/base/TestBase.java
@@ -1,14 +1,28 @@
 package com.tau.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     private static WebDriver driver = null;
 
     public WebDriver getDriver(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/win/chromedriver.exe");
-        driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/win/chromedriver.exe");
+//        driver = new ChromeDriver();
+//        return driver;
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+
         return driver;
     }
 }
